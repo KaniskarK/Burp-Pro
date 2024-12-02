bd953e3b8fa5c86c6d09d9df9c82692d87d0055c
KANISKAR K <76870433+KaniskarK@users.noreply.github.com>
2024-12-02 22:06:30 +0530

    Added Manually

---
+++ burp.ps1
@@ -0,0 +1,27 @@
+# Creating Burp.bat file with command for execution
+if (Test-Path burp.bat) {rm burp.bat}
+$path = "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:`"$pwd\loader.jar`" -noverify -jar `"$pwd\burpsuite_pro_v$version.jar`""
+$path | add-content -path Burp.bat
+echo "`nBurp.bat file is created"
+
+
+# Creating Burp-Suite-Pro.vbs File for background execution
+if (Test-Path Burp-Suite-Pro.vbs) {
+   Remove-Item Burp-Suite-Pro.vbs}
+echo "Set WshShell = CreateObject(`"WScript.Shell`")" > Burp-Suite-Pro.vbs
+add-content Burp-Suite-Pro.vbs "WshShell.Run chr(34) & `"$pwd\Burp.bat`" & Chr(34), 0"
+add-content Burp-Suite-Pro.vbs "Set WshShell = Nothing"
+echo "`nBurp-Suite-Pro.vbs file is created."
+
+# Remove Additional files
+rm Linux_setup.sh
+del -Recurse -Force .\.github\
+
+
+# Lets Activate Burp Suite Professional with keygenerator and Keyloader
+echo "Reloading Environment Variables ...."
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+echo "`n`nStarting Keygenerator ...."
+start-process java.exe -argumentlist "-jar loader.jar"
+echo "`n`nStarting Burp Suite Professional"
+java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:"loader.jar" -noverify -jar "burpsuite_pro_v$version.jar"
